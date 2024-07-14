de5aa38f5dc1b6f37a7590ff103bd1113f138433
Arunachalam M <rome101202@gmail.com>
2024-07-14 17:55:27 +0530

    add food items

---
+++ frontend/src/Context/StoreContext.jsx
@@ -0,0 +1,22 @@
+// We can access this data from any component
+
+import { createContext } from "react";
+import { food_list } from "../assets/assets";
+
+export const StoreContext = createContext(null);
+
+const StoreContextProvider = (props) => {
+
+
+    const contextValue = {
+        food_list
+    }
+
+    return (
+        <StoreContext.Provider value={contextValue}>
+            {props.children}
+        </StoreContext.Provider>
+    )
+}
+
+export default StoreContextProvider;
