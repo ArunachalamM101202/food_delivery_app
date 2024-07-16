f81d9b6d24d315cbbd37e8f1c1f5bf2fec1429ad
Arunachalam M <rome101202@gmail.com>
2024-07-16 21:10:35 +0530

    add storecontext code

--- frontend/src/components/FoodItem/FoodItem.jsx
+++ frontend/src/components/FoodItem/FoodItem.jsx
@@ -1,28 +1,45 @@
 import React from 'react'
 import './FoodItem.css'
 import { assets } from '../../assets/assets'
+import { useContext } from 'react'
+import StoreContextProvider from '../../Context/StoreContext'
+import { StoreContext } from '../../Context/StoreContext'
 
 const FoodItem = ({ id, name, price, description, image }) => {
+
+
+    const { cartItems, addToCart, removeFromCart } = useContext(StoreContext);
+
     return (
         <div className='food-item'>
             <div className='food-item-image-container'>
                 <img className="food-item-image" src={image} alt="" />
+
+                {!cartItems[id]
+                    ? <img className='add' onClick={() => addToCart(id)} src={assets.add_icon_white} />
+                    : <div className='food-item-counter'>
+                        <img onClick={() => removeFromCart(id)} src={assets.remove_icon_red} />
+                        <p>{cartItems[id]}</p>
+                        <img onClick={() => addToCart(id)} src={assets.add_icon_green} />
+                    </div>
+                }
             </div>
             <div className='food-item-info'>
-                <div className="food-item-rating">
+                <div className="food-item-name-rating">
                     <p>
                         {name}
                     </p>
                     <img src={assets.rating_starts}></img>
-                    <p className='food-item-desc'>
-                        {description}
-                    </p>
-                    <p className='food-item-price'>
-                        ${price}
-                    </p>
                 </div>
+                <p className='food-item-desc'>
+                    {description}
+                </p>
+                <p className='food-item-price'>
+                    ${price}
+                </p>
+
             </div>
-        </div>
+        </div >
     )
 }
 
